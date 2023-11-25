1093edfa4f4bb8544bf3cb4ee481ed4908069647
valosekj <jan.valosek@upol.cz>
2023-11-25 11:04:45 -0500

    Add echo

--- loop_across_subjects_to_generate_figure.sh
+++ loop_across_subjects_to_generate_figure.sh
@@ -6,6 +6,7 @@ for file in *perslice_PAM50.csv;do
     subject=${file:0:14}
     session1=${file:4:5}
     session2=${file:9:5}
+    echo "Processing ${subject} ${session1} ${session2} ..."
     python /Users/valosek/code/dcm-brno/generate_figures_PAM50_two_sessions.py \
     -path-HC ${SCT_DIR}/data/PAM50_normalized_metrics \
     -ses1 /Users/valosek/data/results/dcm-brno_2023-11-13/results/${subject}_ses-${session1}_T2w_metrics_perslice_PAM50.csv \

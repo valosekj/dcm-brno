ab7510df91ea146e5f4393944cf5a619be156974
valosekj <jan.valosek@upol.cz>
2024-02-19 16:34:45 -0500

    Use ${HOME}

--- scripts/loop_across_subjects_to_generate_figure.sh
+++ scripts/loop_across_subjects_to_generate_figure.sh
@@ -15,13 +15,13 @@ for file in *perslice_PAM50.csv;do
     session2=${file:9:5}
 
     # Check if the subject is listed in the exclusion list (exclude.yml), if so, skip it
-    if grep -q $subject ../exclude.yml; then
+    if grep -q $subject ${HOME}/code/dcm-brno/exclude.yml; then
         echo "Subject ${subject} is listed in the exclusion list. Skipping."
         continue
     fi
 
     echo "Processing ${subject} ${session1} ${session2} ..."
-    python /Users/valosek/code/dcm-brno/generate_figures_PAM50_two_sessions.py \
+    python ${HOME}/code/dcm-brno/scripts/generate_figures_PAM50_two_sessions.py \
     -path-HC ${SCT_DIR}/data/PAM50_normalized_metrics \
     -ses1 ${PATH_RESULTS}/results/${subject}_ses-${session1}_T2w_metrics_perslice_PAM50.csv \
     -ses2 ${PATH_RESULTS}/results/${subject}_ses-${session2}_T2w_metrics_perslice_PAM50.csv \

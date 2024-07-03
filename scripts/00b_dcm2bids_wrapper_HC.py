2d4f1027900728a6023b6548d30521ff6b6c4393
valosekj <jan.valosek@upol.cz>
2024-07-03 11:14:20 -0400

    Fix example usage

--- scripts/00b_dcm2bids_wrapper_HC.py
+++ scripts/00b_dcm2bids_wrapper_HC.py
@@ -7,9 +7,9 @@
 # runs dcm2bids for each subject.
 #
 # USAGE:
-#     00b_dcm2bids_wrapper.py
-#           -path-in <PATH_TO_DATASET>/sourcedata, e.g., /md3/dcm-brno/sourcedata
-#           -path-out <PATH_TO_DATASET>, e.g., /md3/dcm-brno
+#     00b_dcm2bids_wrapper_HC.py
+#           -path-in <PATH_TO_DATASET>/sourcedata       # e.g., /md3/dcm-brno/sourcedata
+#           -path-out <PATH_TO_DATASET>                 # e.g., /md3/dcm-brno
 #           -transcript-table table.tsv
 #           -dcm2bids-config dcm2bids_config.json
 #

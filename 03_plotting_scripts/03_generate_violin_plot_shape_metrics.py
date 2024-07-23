6e2a12cd181b55b455584e3d9f060dcbb12d5355
valosekj <jan.valosek@upol.cz>
2024-07-23 16:47:04 -0400

    Show all p-values

--- 03_plotting_scripts/03_generate_violin_plot_shape_metrics.py
+++ 03_plotting_scripts/03_generate_violin_plot_shape_metrics.py
@@ -210,11 +210,11 @@ def generate_figure(df, number_of_subjects, vert_level, stats_dict, fname_out):
         axs[index].invert_xaxis()
 
         # If the p-value is less than 0.05, add the significance annotation
-        if stats_dict[metric] < 0.05:
-            axs[index].annotate(f'p{format_pvalue(stats_dict[metric], alpha=.001, include_space=False)}',
-                                xy=(0.5, 0.95), xycoords='axes fraction',
-                                ha='center', va='center',
-                                fontsize=10, color='black')
+        #if stats_dict[metric] < 0.05:
+        axs[index].annotate(f'p{format_pvalue(stats_dict[metric], alpha=.001, include_space=False)}',
+                            xy=(0.5, 0.95), xycoords='axes fraction',
+                            ha='center', va='center',
+                            fontsize=10, color='black')
 
         axs[index].set_xlabel('')
         axs[index].set_ylabel(METRIC_TO_AXIS[metric], fontsize=LABELS_FONT_SIZE)
